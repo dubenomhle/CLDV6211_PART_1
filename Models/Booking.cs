cf058add83c65dc63a8c608fb8876afe09a5a35b
Nomhle Racheal Dube <ST10328670@imconnect.edu.za>
2025-04-03 16:50:10 +0200

    fixed error in booking model

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -1,13 +1,15 @@
-﻿namespace EventEase_Part_1.Models;
+﻿namespace EventEase_Part_1.Models
 
-
-public class Booking
 {
-    public int BookingId { get; set; }
-    public int EventId { get; set; }
-    public int VenueId { get; set; }
-    public DateTime BookingDate { get; set; } = DateTime.Now;
+    public class Booking
+    {
+        public int BookingId { get; set; }
+        public int EventId { get; set; }
+        public int VenueId { get; set; }
+        public DateTime BookingDate { get; set; } = DateTime.Now;
 
-    public Event Event { get; set; }
-    public Venue Venue { get; set; }
+        public Event Event { get; set; }
+        public Venue Venue { get; set; }
+    }
+
 }
