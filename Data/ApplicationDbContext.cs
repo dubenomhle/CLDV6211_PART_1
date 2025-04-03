a69a82639615fd45f96fc726dcd7a10c2064e207
Nomhle Racheal Dube <ST10328670@imconnect.edu.za>
2025-04-03 16:44:05 +0200

    added data folder then added ApplicationDbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EventEase_Part_1.Models;
+using System.Collections.Generic;
+namespace EventEase_Part_1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Venue> Venues { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
+
