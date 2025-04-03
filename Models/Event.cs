977116574880de0d3dcbd2977f227249ba4b2126
Nomhle Racheal Dube <ST10328670@imconnect.edu.za>
2025-04-03 16:51:04 +0200

    made event changes

--- Models/Event.cs
+++ Models/Event.cs
@@ -1,10 +1,13 @@
-﻿namespace EventEase_Part_1.Models; 
-public class Event
+﻿namespace EventEase_Part_1.Models
 {
-    public int EventId { get; set; }
-    public string Name { get; set; }
-    public DateTime Date { get; set; }
-    public string Description { get; set; }
-    public int? VenueId { get; set; }
-    public Venue Venue { get; set; }
+    public class Event
+    {
+        public int EventId { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public int? VenueId { get; set; }
+        public Venue Venue { get; set; }
+    }
 }
+
