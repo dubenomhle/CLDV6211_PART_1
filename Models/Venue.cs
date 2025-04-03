5e400bb8a93c04cfc3b53eda5fed14085283fb93
Nomhle Racheal Dube <ST10328670@imconnect.edu.za>
2025-04-03 16:53:07 +0200

    changed code on venue model

--- Models/Venue.cs
+++ Models/Venue.cs
@@ -1,11 +1,14 @@
-﻿namespace EventEase_Part_1.Models;
-public class Venue
+﻿namespace EventEase_Part_1.Models
 {
-    public int VenueId { get; set; }
-    public string Name { get; set; }
-    public string Location { get; set; }
-    public int Capacity { get; set; }
-    public string ImageUrl { get; set; }
+    public class Venue
+    {
+        public int VenueId { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int Capacity { get; set; }
+        public string ImageUrl { get; set; }
 
-    public List<Event> Events { get; set; } = new List<Event>();
+        public List<Event> Events { get; set; } = new List<Event>();
+    }
+
 }
