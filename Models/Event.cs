95a95d86546808d55c2979655a6d2c3a4f8dc169
Nomhle Racheal Dube <ST10328670@imconnect.edu.za>
2025-06-18 14:49:23 +0200

    UPDATED THE EVENTTYPE AND EVENT MODEL

--- Models/Event.cs
+++ Models/Event.cs
@@ -7,20 +7,20 @@ namespace EventEase_Part_1.Models
         public int EventID { get; set; }
 
     
-        public string EventName { get; set; }
+        public string? EventName { get; set; }
 
        
         public DateTime EventDate { get; set; }
 
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
         
-        public int VenueID { get; set; }
+        public int? VenueID { get; set; }
 
 
-        public Venue Venue { get; set; }
-        public int EventTypeID { get; set; }
-        public EventType EventType { get; set; }
+        public Venue? Venue { get; set; }
+        public int? EventTypeID { get; set; }
+        public EventType? EventType { get; set; }
 
     }
 
